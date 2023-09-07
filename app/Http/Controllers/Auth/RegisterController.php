3cbf5e0e21d999f78bb9858192dd667fb115a6f9
adeliaeka <adellia3210@gmail.com>
2023-09-07 14:17:33 +0700

    first commit

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+use App\Models\User;
+use Inertia\Inertia;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RegisterController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Auth/Register');
+    }
+    public function store(Request $request)
+    {
+        /**
+         * validate request
+         */
+        $this->validate($request, [
+            'name'      => 'required',
+            'email'     => 'required|unique:users',
+            'password'  => 'required|confirmed'
+        ]);
+
+        /**
+         * create user
+         */
+        User::create([
+            'name'      => $request->name,
+            'email'     => $request->email,
+            'password'  => bcrypt($request->password)
+        ]);
+
+        //redirect
+        return redirect('/login')->with('status', 'Register Berhasil!');
+    }
+}
